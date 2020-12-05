f5bed775ef0c0522466af235234778e9fbdc0873
albert.jiang <418194983@qq.com>
2020-12-05 18:13:58 +0800

    no message

--- docs/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/jiangjianyuan/work_space/组件库/wx-component/docs/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
